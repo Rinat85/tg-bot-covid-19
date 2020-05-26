fdad88dd9bb3b674831ede1f55271b4b406387ba
web_dev_85 <pacmanreal@gmail.com>
2020-05-26 19:18:07 +0500

    fix: buttons v1

--- bot.js
+++ bot.js
@@ -7,40 +7,41 @@ const COUNTRIES_LIST = require('./constants');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) =>
-    ctx.reply(
-        `
+  ctx.reply(
+    `
 Привет ${ctx.message.from.first_name + ' ' + ctx.message.from.last_name}
 Узнай статистику по коронавирусу.
 Введи на английском название страны и получи статистику.
 Посмотреть весь список стран можно командой /help.
 `,
-        Markup.keyboard([
-            ['Russia', 'Uzbekistan'],
-            ['US', 'Canada'],
-        ])
-        .resize()
-        .extra()
-    )
+    Markup.keyboard([
+      ['Russia', 'Uzbekistan'],
+      ['US', 'Canada'],
+    ])
+      .oneTime()
+      .resize()
+      .extra()
+  )
 );
 
 bot.help((ctx) => ctx.reply(COUNTRIES_LIST));
 
 bot.on('text', async (ctx) => {
-    let data = {};
+  let data = {};
 
-    try {
-        data = await api.getReportsByCountries(ctx.message.text);
+  try {
+    data = await api.getReportsByCountries(ctx.message.text);
 
-        const formatedData = `
+    const formatedData = `
     <b>Страна: ${data[0][0].country}</b>\n
     Случаи: ${data[0][0].cases}
     Смертей: ${data[0][0].deaths}
     Выздоровевших: ${data[0][0].recovered}
     `;
-        ctx.reply(formatedData);
-    } catch {
-        ctx.reply('Вы неправильно написали название страны. Посмотрите /help');
-    };
+    ctx.reply(formatedData);
+  } catch {
+    ctx.reply('Вы неправильно написали название страны. Посмотрите /help');
+  }
 });
 
 bot.launch();
